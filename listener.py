a223a38416f5c17090a2c8170daa02926eae2904
C0dy801 <santiagotorres8012@gmail.com>
2026-02-07 19:10:22 -0600

    Refactor listener.py to improve connection handling

--- listener.py
+++ listener.py
@@ -1,40 +1,39 @@
+from colorama import Fore
 import socket as soc
+from time import sleep
 import argparse
-import time
 
-
-#Argumentos
-parser = argparse.ArgumentParser(description="Conexion listener!")
-
-#Argumentos del host y puerto
-parser.add_argument('--host',required=True,type=str,help="Conexion al host ejemplo: --host 127.0.0.1")
-parser.add_argument('--port',required=True,type=int,help="Conexion del puerto ejemplo: --port 4444")
-
-Parsers = parser.parse_args()
-
-
+msg = 'Loading...'
+print(Fore.GREEN+msg)
+sleep(1)
+Parser = argparse.ArgumentParser(description="Conexion listener!")
 #Conexion
-conexion = soc.socket(soc.AF_INET,soc.SOCK_STREAM)
-if Parsers.host and Parsers.port:
-    try:
-        serv = conexion.connect_ex((Parsers.host,Parsers.port))
-        print(f"Conexion del host:{Parsers.host}, Puerto en:{Parsers.port}")
-        while True:
-            msg = conexion.recv(4096).decode('utf-8')
-            if msg.lower()=="exit":
-                print("Conexion Terminada")
-                break
-            print(msg)
+def conexion(ip,port):
+    with soc.socket(soc.AF_INET,soc.SOCK_STREAM) as conn:
+        try:
+            conn.connect_ex((ip,port))
+            print(f"Conexion en la ip:{ip} y puerto:{port}")
+            while True:
+                serv = conn.recv(4096).decode('utf-8')
+                print(f"Serv: {serv}")
+                if serv.lower()=="exit":
+                    break
+        #Manejo de errores
+        except soc.timeout:
+            print("Conexion agotada")
+            sleep(1)
+        except ConnectionError as e:
+            print(f"Problemas con: {e}")
+        except Exception as e:
+            print(f"Error: {e}")
 
-    #Manejo de errores
-    except soc.timeout:
-        print("Conexion agotada")
-        time.sleep(1)
-    except ConnectionError as e:
-        print(f"Problemas con: {e}")
-        time.sleep(1)
-    except Exception as e:
-        print(f"Problemas con {e}")
-        time.sleep(1)
-else:
-    print("Debe poner un host y un puerto especifico para conectarse! ejemplos: --host 127.0.0.1 --port 4444")
+
+
+if __name__ == "__main__":
+    host = Parser.add_argument('--host',required=True,help="Conexion al host ejemplo: --host 127.0.0.1")
+    port = Parser.add_argument('--port',required=True,help="Conexion del puerto ejemplo: --port 4444")
+    args = Parser.parse_args()
+    if args.host and args.port:
+        conexion(args.host,args.port)
+    else:
+        print("Debe poner un host y un puerto especifico para conectarse! ejemplos: --host 127.0.0.1 --port 4444")
